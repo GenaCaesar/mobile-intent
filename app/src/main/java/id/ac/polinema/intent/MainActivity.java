788e9bb902333004395cf5a756a313857a3d2271
Gena Caesar Athallah <genac75@gmail.com>
2020-02-20 09:37:06 +0700

    Praktikum Parcelable

--- app/src/main/java/id/ac/polinema/intent/MainActivity.java
+++ app/src/main/java/id/ac/polinema/intent/MainActivity.java
@@ -43,4 +43,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, BundleActivity.class);
         startActivity(intent);
     }
+
+    public void handleParcelable(View view) {
+        Intent intent = new Intent(this, BundleActivity.class);
+        startActivity(intent);
+    }
 }
